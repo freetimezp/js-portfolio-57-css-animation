1ea370c46ec22589ae0b8e4da086a930778b98e4
freetimezp <kraftukrnet@ukr.net>
2024-03-12 21:20:31 +0200

    07. js-portfolio-57-css-animation. Menu section. Toggle menu.

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+let menuInside = document.querySelector(".menu-inside");
+let menu = document.querySelector(".menu");
+
+menuInside.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    console.log(123);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
